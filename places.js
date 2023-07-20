fce9b8152bc5b0344f32f5dffc1a47251011146d
dpamar <dpamar@gmail.com>
2023-07-20 13:28:22 +0200

    Update places.js

--- places.js
+++ places.js
@@ -1,3 +1,6 @@
 var places = [
+    [45.180905, 5.722278, 'Viens ici pour te dire « David préparera Chabbat en rentrant ». Profite de cette pensée 2 minutes et apprécie ta soirée pleinement !'],
+    [45.18072, 5.722285, 'Petit conseil : mets des baskets !'],
+    [45.180712, 5.722308, 'On se retrouve en bas']
 ];
-
+//Resume game designer URL: https://dpamar.github.io/sortie2007/generator.html?places=W1s0NS4xODA5MDUsIDUuNzIyMjc4LCAnVmllbnMgaWNpIHBvdXIgdGUgZGlyZSCroERhdmlkIHBy6XBhcmVyYSBDaGFiYmF0IGVuIHJlbnRyYW50oLsuIFByb2ZpdGUgZGUgY2V0dGUgcGVuc+llIDIgbWludXRlcyBldCBhcHBy6WNpZSB0YSBzb2ly6WUgcGxlaW5lbWVudCAhJ10sWzQ1LjE4MDcyMCwgNS43MjIyODUsICdQZXRpdCBjb25zZWlsIDogbWV0cyBkZXMgYmFza2V0cyAhJ10sWzQ1LjE4MDcxMiwgNS43MjIzMDgsICdPbiBzZSByZXRyb3V2ZSBlbiBiYXMnXV0=
